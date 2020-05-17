b0aac550ea5e2e1f4f3c95469a213053af50ea3b
carloseduardoe <42809876+carloseduardoe@users.noreply.github.com>
2020-05-17 00:13:02 -0500

    temp database

--- src/TempDatabase.js
+++ src/TempDatabase.js
@@ -1,3 +1,17 @@
-module.exports = {
-    users: []
+class TempDatabase {
+    constructor() {
+        this.users = [];
+        this.tokens = {};
+
+        setInterval(() => {
+            Object.keys(this.tokens).forEach(item => {
+                if (this.tokens[item].count)
+                    item.count--;
+                else
+                    delete this.tokens[item];
+            });
+        }, 1000);
+    }
 }
+
+module.exports = TempDatabase;
